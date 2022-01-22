9b8fc38e6473d55a446e8b502b3d100382dbf7b8
Professor Agora <agoraprofessor@gmail.com>
2022-01-22 16:22:30 +0530

    Update help.py

--- RiZoeLXSpam/plugins/help.py
+++ RiZoeLXSpam/plugins/help.py
@@ -4,9 +4,9 @@ from telethon.tl.custom import button
 from time import time
 from datetime import datetime
     
-HELP_PIC = "https://telegra.ph/file/9acc785291052c8f8998d.jpg"
+HELP_PIC = "https://te.legra.ph/file/8f38c2a68e7a4c2e3f6c9.jpg"
 
-Riz_Help = "🔥 𝗥𝗜𝗭𝗢𝗘𝗟 𝗫 𝗦𝗣𝗔𝗠 🔥\n\n"
+Riz_Help = "🔥 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 𝗦𝗣𝗔𝗠𝗦𝗧𝗘𝗥𝗦 🔥\n\n"
  
 Riz_Help += f"__ᴄᴍᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ʀɪᴢᴏᴇʟ x sᴘᴀᴍ__\n\n"
 
@@ -24,7 +24,7 @@ Riz_Help += f" `.raid` - to raid\n `.replyraid` - to active reply raid\n `.drepl
  
 Riz_Help += f"ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ.\n\n"
 
-Riz_Help += f"© @RiZoeLX | @DNHxHELL\n"
+Riz_Help += f"© @Agoraswamy_Professor | @Agora_Spam_Official\n"
 
 
 @Riz.on(events.NewMessage(pattern=".help"))
@@ -35,10 +35,10 @@ async def help(event):
                                   caption=Riz_Help,
                                   buttons=[
         [
-        Button.url("ᴀʟʟ ᴄᴍᴅs", "https://telegra.ph/%F0%9D%97%A5%F0%9D%97%9C%F0%9D%97%AD%F0%9D%97%A2%F0%9D%97%98%F0%9D%97%9F-%F0%9D%97%AB-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0-11-28-2")
+        Button.url("ᴀʟʟ ᴄᴍᴅs", "https://te.legra.ph/%F0%9D%97%A3%F0%9D%97%A5%F0%9D%97%A2%F0%9D%97%99%F0%9D%97%98%F0%9D%97%A6%F0%9D%97%A6%F0%9D%97%A2%F0%9D%97%A5-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0%F0%9D%97%A6%F0%9D%97%A7%F0%9D%97%98%F0%9D%97%A5%F0%9D%97%A6-01-22"
         ],
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/RiZoeLX")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/Agoraswamy_Professor")
         ] 
         ]
         )                                                         
