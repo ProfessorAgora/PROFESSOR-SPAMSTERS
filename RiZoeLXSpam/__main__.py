eee84b81f4df8f1e5d9bef03b4d7b3cc30a11899
Professor Agora <agoraprofessor@gmail.com>
2022-01-22 16:52:25 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -22,8 +22,8 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("RiZoeL Bot Spam Successfully deployed -!")
-print("Enjoy! Do visit @RiZoeLX")
+print("Professor Spamsters Successfully deployed -!")
+print("Enjoy! Do visit @Agora_Spam_Official")
 
 if __name__ == "__main__":
     Riz.run_until_disconnected()
